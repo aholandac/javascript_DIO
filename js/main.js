046dcc65755d6fa0ab6388f1c6d82fe5b9c8ce93
aholandac <aholandac@hotmail.com>
2021-06-25 16:03:24 -0300

    Novos comandos em main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-alert('Hello JavaScript');
+//alert('Hello JavaScript');
 
 var nome = 'André Holanda';
 var idade = 41;
 var n1 = 5;
-alert('Bem Vindo '+nome+', Idade: '+idade);
+//alert('Bem Vindo '+nome+', Idade: '+idade);
 
 console.log(nome);
 console.log(idade+idade);
@@ -16,5 +16,20 @@ console.log(frase.toLowerCase());
 console.log(frase.replace('Japão','Brasil').toUpperCase());
 console.log(frase.replace('Japão', 'CHINA').toLowerCase());
 
-alert(idade*n1);
+//alert(idade*n1);
 console.log(idade*n1);
+
+var lista = ['maçã', 'pera', 'laranja']
+console.log(lista[1]);
+console.log(lista[0],lista[1],lista[2]);
+lista.push('melã');
+console.log(lista[3]);
+console.log(lista[0],lista[1],lista[2], lista[3]);
+lista.pop();
+console.log(lista[0],lista[1],lista[2], lista[3]);
+console.log(lista.length);
+lista.push('Abacaxi');
+console.log(lista.length);
+console.log(lista[0],lista[1],lista[2], lista[3]);
+console.log(lista);
+console.log(lista.toString());
